1af3e361f4619632fc3870070b90865623c7587e
Dmitry <inaumovd@gmail.com>
2018-07-10 21:55:24 +0800

    Добавлены сжатые стили и скрипты

---
+++ js/main-min.js
@@ -0,0 +1,1 @@
+var button=document.querySelector(".hotel-search-btn");var form=document.querySelector(".search-form");button.addEventListener("click",function(evt){evt.preventDefault();form.classList.toggle("search-form-hide")})
